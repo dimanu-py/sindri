be930f63a0ddd03b2734ba802de828c79b2087dd
dimanu-py <dimanu.py@gmail.com>
2025-09-30 23:07:31 +0200

    refactor(value-objects): modify validate decorator to allow order of validation

--- value_object/value_objects/decorators/validation.py
+++ value_object/value_objects/decorators/validation.py
@@ -1,7 +1,26 @@
-from typing import Callable, Any
+from collections.abc import Callable
+from typing import Any
 
 
-def validate(func: Callable[..., Any]) -> Callable[..., Any]:
-    """Mark a method as a validator for ValueObject validation."""
-    setattr(func, "_is_validator", True)
-    return func
+def validate[F: Callable[..., Any]](func: F | None = None, *, order: int = 0) -> Callable[[F], F] | F:
+    """Mark a method as a validator for ValueObject validation.
+
+    Arguments:
+        func: the function to decorate.
+        order: order in which this validator should run relative to other validators in the same class. Lower numbers run first.
+    """  # noqa: E501
+
+    def wrapper(fn: F) -> F:
+        if type(order) is not int:
+            raise TypeError(f"Validation order {order} must be an integer. Got {type(order).__name__} type.")
+        if order < 0:
+            raise ValueError(f"Validation order {order} must be a positive value.")
+
+        fn._is_validator = True
+        fn._order = order
+        return fn
+
+    if func is not None:
+        return wrapper(func)
+
+    return wrapper
