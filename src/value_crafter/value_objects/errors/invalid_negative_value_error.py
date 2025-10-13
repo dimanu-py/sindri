dc8d6ee23e909d2157a1238feb8e9d4c2efb5cb8
dimanu-py <dimanu.py@gmail.com>
2025-10-13 16:27:04 +0200

    feat(errors): remove unused error

--- src/value_crafter/value_objects/errors/invalid_negative_value_error.py
+++
@@ -1,8 +0,0 @@
-from src.value_crafter.value_objects.errors.value_object_validation_error import ValueObjectValidationError
-
-
-class InvalidNegativeValueError(ValueObjectValidationError):
-    def __init__(self, value: int) -> None:
-        super().__init__(
-            message=f"Invalid negative value: {value}",
-        )
