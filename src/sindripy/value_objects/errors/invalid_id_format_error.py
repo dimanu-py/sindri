2fd6840407f98fd9a3a708eee3137255cbebfaad
dimanu-py <dimanu.py@gmail.com>
2026-05-30 01:21:14 +0200

    refactor: remove old src/sindripy monolith after package extraction

--- src/sindripy/value_objects/errors/invalid_id_format_error.py
+++
@@ -1,8 +0,0 @@
-from sindripy.value_objects.errors.sindri_validation_error import SindriValidationError
-
-
-class InvalidIdFormatError(SindriValidationError):
-    def __init__(self) -> None:
-        super().__init__(
-            message="Id must be a valid UUID",
-        )
--- src/sindripy/value_objects/errors/required_value_error.py
+++
@@ -1,8 +0,0 @@
-from sindripy.value_objects.errors.sindri_validation_error import SindriValidationError
-
-
-class RequiredValueError(SindriValidationError):
-    def __init__(self) -> None:
-        super().__init__(
-            message="Value is required, can't be None",
-        )
